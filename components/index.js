e91b424e9e0efcf468891e7e724b129642986dc3
Alina Krasivskaya <linakrasivskaya@yandex.ru>
2022-03-29 22:23:21 +0300

    js для попапа добавления фото

--- components/index.js
+++ components/index.js
@@ -1,36 +1,53 @@
 window.addEventListener('load', () => {
-    const popupElement = document.querySelector('.popup');
+    const editPopup = document.querySelector('.edit__popup');
+    const addPopup = document.querySelector('.add__popup');
     const editButton = document.querySelector('.profile__inner-edit-btn');
     const closeButton = document.querySelector('.popup__close-btn');
-    let formElement = document.querySelector('form');
-    let nameInput = document.querySelector('.popup__input_type_name');
-    let jobInput = document.querySelector('.popup__input_type_job');
-    let nameValue = document.querySelector('.profile__inner-name');
-    let jobValue  = document.querySelector('.profile__inner-activity');
+    const formElement = document.querySelector('form');
+    const nameInput = document.querySelector('.popup__input_type_name');
+    const jobInput = document.querySelector('.popup__input_type_job');
+    const nameValue = document.querySelector('.profile__inner-name');
+    const jobValue  = document.querySelector('.profile__inner-activity');
+    const addButton = document.querySelector('.profile__add-btn');
+    const placeInput = document.querySelector('.popup__input_type_place');
+    const urlInput = document.querySelector('.popup__input_type_url');
 
-    function openPopup() {
-        popupElement.classList.add('popup_opened');
+    function openEditPopup() {
+        editPopup.classList.add('popup_opened');
         nameInput.value = nameValue.textContent;
         jobInput.value = jobValue.textContent;
     }
 
-    function closePopup() {
-        popupElement.classList.remove('popup_opened');
+    function closeEditPopup() {
+        editPopup.classList.remove('popup_opened');
     }
 
-    editButton.addEventListener('click', openPopup);
+    editButton.addEventListener('click', openEditPopup);
 
-    closeButton.addEventListener('click', closePopup);
+    closeButton.addEventListener('click', closeEditPopup);
 
     function formSubmitHandler (evt) {
         evt.preventDefault(); 
         
         nameValue.textContent = nameInput.value;
         jobValue.textContent = jobInput.value;
-        closePopup();
-
+        closeEditPopup();
     }
 
     formElement.addEventListener('submit', formSubmitHandler); 
 
+    function openAddPopup() {
+        addPopup.classList.add('popup_opened');
+    }
+
+    function closeAddPopup() {
+        addPopup.classList.remove('popup_opened');
+    }
+
+    addButton.addEventListener('click', openAddPopup);
+    
+    closeButton.addEventListener('click', closeAddPopup);
+
 });
+
+
