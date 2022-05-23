b830456b316c6b0c1e0cb8e51ece36b9a3276354
Alina Krasivskaya <linakrasivskaya@yandex.ru>
2022-05-23 13:35:31 +0300

    Создать класс UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,18 @@
+export class UserInfo {
+    constructor ({nameValueSelector, jobValueSelector}) {
+        this._nameValue = document.querySelector(nameValueSelector);
+        this._jobValue = document.querySelector(jobValueSelector);
+    }
+
+    getUserInfo () {
+        const data = {}
+        data.username = this._nameValue.textContent;
+        data.job = this._jobValue.textContent;
+        return data; // Объект с данными пользователя; подставляется в форму при открытии
+    }
+
+    setUserInfo (data) { // Метод  принимает новые данные пользователя и добавляет их на страницу
+        this._nameValue.textContent = data.username;
+        this._jobValue.textContent = data.job;
+    }
+}
