d095878d6c62e9c5546a6ea5a2f26846b9d65d84
Alina Krasivskaya <linakrasivskaya@yandex.ru>
2022-06-09 19:44:50 +0300

    Новые функции для карточки

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,35 +26,76 @@ const createUserInfo = new UserInfo(userInf);
 const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
   headers: {
-      authorization: 'c49772d7-9370-4a4f-bf19-05cc76e5748b',
-      'Content-Type': 'application/json'
+    authorization: 'c49772d7-9370-4a4f-bf19-05cc76e5748b',
+    'Content-Type': 'application/json'
   }
 });
 
 const pageContent = [api.getProfileInfo(), api.getCards()]; // Получить информацию о пользователе и карточки с сервера (массив с промисами)
 
-Promise.all ( pageContent ) // Передать массив с промисами методу Promise.all
+Promise.all(pageContent) // Передать массив с промисами методу Promise.all
   .then(([userStats, data]) => {
     userId = userStats._id;
     createUserInfo.setUserInfo(userStats);
     createCard.renderItems(data);
   })
-  .catch ((err) => {
-    console.log (err);
-})
+  .catch((err) => {
+    console.log(err);
+  })
 
 const zoomPopupFunc = new PopupWithImage('.zoom-popup'); // экземпляр класса PopupWithImage
 zoomPopupFunc.setEventListeners();
 
+const deletePopup = new PopupDeleteImage( // экземпляр класса PopupDeleteImage
+  {
+    callbackSubmitForm: (data, element, id) => { // Функция удаления карточки
+      api.deleteCard(data, id) //  Обращаться к api и удалять из вёрстки элемент карточки
+        .then((data) => { // Если всё ок
+          element.remove(); // Удалить элемент
+          deletePopup.close(); // Закрыть попап
+        })
+        .catch((err) => { // Если не ок
+          console.log(err);
+        })
+    }
+  },
+  '.delete-popup');
+deletePopup.setEventListeners();
 
-// ФУНКЦИЯ ОТКРЫТИЯ ЗУМ-ПОПАПА ДЛЯ ПЕРЕДАЧИ В Card (ИЗ РЕВЬЮ)
-function handleCardClick(cardname, link) {
+
+// Функции, передаваемые в Card
+function handleCardClick(cardname, link) { // Открытие зум-попапа
   zoomPopupFunc.open(cardname, link);
 }
 
-// НОВЫЙ КОД ЗАГРУЗКИ И ДОБАВЛЕНИЯ КАРТОЧЕК
+function deleteCardPopup (cardElement, id) { // Открытие попапа удаления карточки
+  deletePopup.open(cardElement, id);
+}
+
+function likeCards (cardElement, id) { // Поставить лайк
+  api.likeCard (cardElement, id)
+    .then ((data) => {
+      cardElement.querySelector('.photo-gallery__like-btn').classList.add('photo-gallery__like-btn_active'); // Активный лайк
+      cardElement.querySelector('.photo-gallery__like-counter').textContent = data.likes.length; // Счётчик лайков
+    })
+    .catch ((err) => {
+      console.log (err);
+   })
+}
+
+function disiikeCards(cardElement, id) { // Убрать лайк
+  api.dislikeCard(cardElement, id)
+    .then((data) => {
+      cardElement.querySelector('.photo-gallery__like-btn').classList.remove('photo-gallery__like-btn_active');
+      cardElement.querySelector('.photo-gallery__like-counter').textContent = data.likes.length;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
 const createNewCard = (data) => { // Функция создания карточки
-  const card = new Card({ data, handleCardClick }, '#cards');
+  const card = new Card({ data, handleCardClick, deleteCardPopup, likeCards, disiikeCards}, userId, '#cards');
   const cardElement = card.generateCard();
   return cardElement;
 };
